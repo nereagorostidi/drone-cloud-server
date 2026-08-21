9751ae646f70fa637ce8025a61cdc4db58121948
Nerea <nerea.gorostidi.garcia@gmail.com>
2026-08-21 11:04:57 +0200

    Conversión página cotrol a PWA

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,7 @@
  cp ../www/web/journal.html /var/www/html/journal.html
  cp ../www/control/index.html /var/www/control/index.html
  cp ../www/control/estilos.css /var/www/control/estilos.css
+ cp ../www/control/manifest.json /var/www/control/manifest.json
+ cp ../www/control/sw.js /var/www/control/sw.js
+ mkdir -p /var/www/control/icons
+ cp ../www/control/icons/*.png /var/www/control/icons/
